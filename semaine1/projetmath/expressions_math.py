d774f423d930ce4cd4c547a760da33da07a7f9dc
6327393 <6327393@cegepoutaouais.qc.ca>
2026-08-25 10:52:05 -0400

    exercice geometrie

---
+++ semaine1/projetmath/expressions_math.py
@@ -0,0 +1,14 @@
+#expressions mathématiques
+b = 4
+h = 3
+a = b * h / 2
+d = (b**2 + h**2) ** 0.5
+print(a)
+print(d)
+
+
+#expressions logiques
+e1 = d > b and d > h
+e2 = a > 10 or a < 5
+print(e1)
+print(e2)
---
+++ semaine1/projetmath/types_vartiables.py
@@ -0,0 +1,12 @@
+nombre_entier = 10
+nombre_decimal = 12.5
+texte = "allo" # guillemet doubles ou simples
+string = 'bonjour' # str =  chaine = texte
+vrai_faux = True # bool = booléen
+boolean = False
+print(nombre_entier)
+print(nombre_decimal)
+print(texte)
+print(string)
+print(vrai_faux)
+print(boolean)
