64bd628ac297b71c04be6a0b67f0383514d3844a
6327393 <6327393@cegepoutaouais.qc.ca>
2026-08-25 08:44:38 -0400

    Complété ma présentation

---
+++ presentations_andregoumou.py
@@ -0,0 +1,23 @@
+nom = "andre"
+age = 17
+ecole_secondaire = "mont-bleu"
+programme = "Réseau et cybersécurité"
+attentes = "devenir le meilleur technicien"
+besoins = "comprendre mieu la programmation"
+motivations = "connaisance"
+defis = "programmation"
+forces = "ma mémoire"
+loisirs = " jouer au basketball"
+# -------------------
+# --- Ne pas modifier la sectoin du bas ---
+print("===== FICHE APPRENANT =====")
+print("Bonjour, je m’appelle :", nom)
+print("J'ai", age, "ans")
+print("Je suis diplômé de :", ecole_secondaire)
+print("Je suis inscrit en :", programme)
+print("Mes attentes sont :", attentes)
+print("De quoi j’ai besoin :", besoins)
+print("Ce qui me motive :", motivations)
+print("Mes défis :", defis)
+print("Mes forces :", forces)
+print("Entre mes cours, j'aime :", loisirs)
