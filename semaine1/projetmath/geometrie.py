c16c8bfae57a1eb2800307ae082fdb249979ff8c
6327393 <6327393@cegepoutaouais.qc.ca>
2026-08-25 09:35:48 -0400

    exercice aire

---
+++ semaine1/projetmath/geometrie.py
@@ -0,0 +1,14 @@
+"""
+calculer l'aire d'un rectangle
+base
+hauteur
+aire = base*hauteur
+
+"""
+# print(6 * 4)
+# a = 6 * 4
+# print(a)
+base = 6
+hauteur = 4
+aire = base * hauteur
+print("L'aire du rectangle est :", aire)
