f8a2632802030cc9911101f23bf6fa8fe65c4857
andre <6327393@cegepoutaouais.qc.ca>
2026-09-01 10:51:51 -0400

    exercices

---
+++ semaine2/exercices/entree_sorties.py
@@ -0,0 +1,18 @@
+###################################################
+#Entrées: l'adresse IP, le masque de sous-réseau
+#Sorties: Nombre de bits pour le réseau
+# Nombre de bits pour les hôtes
+# Nombre total d'adresses
+# Nombre d'hôtes utilisables
+#Formules: Nb_bits_reseau = Masque
+# Nb_bits_hotes = 32- Masque
+# Nb_adresses = 2 ** Nb_bits_hotes
+# Nb_hotes_util = 2 ** Nb_bits_hotes - 2
+# Adresse_IP, Masque Nb_bits_reseau
+# Nb_bits_hotes
+# Nb_adresses
+# Nb_hotes_util
+####################################################
+
+def
+
