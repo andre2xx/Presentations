a09d47d025d1a21b8574615ff4e7073598aaa4eb
andre <6327393@cgepoutaouais.qc.ca>
2026-09-01 11:49:15 -0400

    exercices

--- semaine2/exercices/entree_sorties.py
+++ semaine2/exercices/entree_sorties.py
@@ -14,5 +14,29 @@
 # Nb_hotes_util
 ####################################################
 
-def
+def calculer_parametres_sous_reseau():
+    """
+    permet de calculé les informations de masque de sous réseau
+    :return:
+    """
+    adresse_ip = input("Entrer l'adresse ip : ")
+    masque = input("entrer le maseque : ")
+
+    nb_bits_reseau = masque
+    nb_bits_hotes = 32 -masque
+    nb_adresse = 2** nb_bits_hotes
+    nb_hotes_util =2** nb_bits_hotes-2
+
+    print(nb_bits_reseau)
+    print(nb_bits_hotes)
+    print(nb_adresse)
+    print(nb_hotes_util)
+
+calculer_parametres_sous_reseau()
+
+
+
+
+
+
 
