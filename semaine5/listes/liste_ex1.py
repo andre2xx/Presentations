234fa601bddfb5efe1c42553e891a6fd05f9b93c
6327393 <6327393@cegepoutaouais.qc.ca>
2026-09-17 17:50:59 -0400

    listes

---
+++ semaine5/listes/liste_ex1.py
@@ -0,0 +1,17 @@
+couleur = ["bleu", "jaune", "rouge", "vert","bleu", "jaune"]
+print(couleur)
+couleur.pop(1)
+couleur.insert(1, "rouge")
+print(couleur)
+couleur.append("vert")
+print(couleur)
+couleur.insert(2,"bleu")
+print(couleur)
+couleur.append("rouge",)
+couleur.append("vert")
+print(couleur)
+couleur.remove("bleu")
+
+
+
+
