1b5117a0507bb941e0d7fe9989ede6f93750e506
6327393 <6327393@cegepoutaouais.qc.ca>
2026-09-22 10:52:05 -0400

    exercice chasse au tresor

---
+++ semaine5/chasse_au_trésor/chasse_au_tresor.py
@@ -0,0 +1,20 @@
+import random
+
+##########################################################
+#                     pseudocode
+
+# sac = [potions scintillantes, clés mystérieuses, boules brillantes, squelettes miniatures]
+#explorer():
+#   ajouter objet au sac
+#   Si len(sac) >= 15
+#       trier sac
+#trier_sac: permet de trier le sac de zigomar
+#   choix =
+#tri_automatique: retire tous les objets dont le nom contient un chiffre ou la lettre "b" ou chiffre
+#   Si objet dans sac contient "b"
+#       retirer objet de sac
+#tri_manuel: demande l'utilisateur quel objet il veux qu’elle retire pour continuer sa chasse.
+#   retirer objet
+#            *chaase terminé*
+#melanger objet du sac -> random.shuffle(sac)
+#attrapper un objet au hasard -> random.choice(sac)
